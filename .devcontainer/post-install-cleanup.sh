5e4b9df684bdd2f95642ed500fccae43c1e84b0d
Armin Aschoff <105582183+AshieSlashy@users.noreply.github.com>
2024-04-25 21:27:59 +1000

    Make post-install executable & more generic

--- .devcontainer/post-install-cleanup.sh
+++ .devcontainer/post-install-cleanup.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
 
 REQUIREMENTS_FILE=requirements.txt
+echo '================================================='
 echo "Installing $REQUIREMENTS_FILE"
 if [ -f "$REQUIREMENTS_FILE" ]; then
     pip3 install -r "$REQUIREMENTS_FILE"
     rm "$REQUIREMENTS_FILE"
+else
+    echo "Didn't find $REQUIREMENTS_FILE, skipping..."
 fi
-
-WHEEL_FILES='*.whl'
-echo "Installing $WHEEL_FILES"
-if [ -f "$WHEEL_FILES" ]; then
-    pip3 install "$WHEEL_FILES"
-    rm "$WHEEL_FILES"
-fi
+echo '================================================='
+echo "Installing all whl files"
+for F in *.whl; do
+    if [[ ! -f $F ]]; then
+        echo "Didn't find any whl files, skipping..."
+        break; 
+    fi
+    echo "Installing $F..."
+    pip3 install "$F"
+    rm "$F"
+done
+echo '================================================='
